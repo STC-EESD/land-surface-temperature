eae83b0c38879db076cd523f114492097cbda38f
pgosztonyi <patrick.gosztonyi@statcan.gc.ca>
2023-01-06 19:51:03 +0000

    Update to LST Image download script - Changed the method for defining the output projection CRS from a CRS WKT string using ee.Reporject, to casting the WKT CRS into an ee.Projection object and using the "crs" option when exporting the data to Google Drive (may perform slghtly better than manually reprojecting, and results are the same)
    - Also modified the comments to properly reflect the contents of the
    script

--- 902-pixel-daily-time-series/code/test_eeBatchExport.py
+++ 902-pixel-daily-time-series/code/test_eeBatchExport.py
@@ -12,18 +12,11 @@ def test_eeBatchExport(google_drive_folder):
     print( "\n########## " + thisFunctionName + "() starts ..." )
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    # setting the dates for testing the script
-    #date1 = ee.Date.fromYMD(2022,07,14) # reasonably clear day for most of Canada
-    #date2 - date1.advance(1,"day") #only get one image for download
     
     # Get the MODIS LST 1km Daily dataset (From Terra satellite)
     t_modis = ee.ImageCollection("MODIS/061/MOD11A1") \
-                .filterDate(ee.Date("2022-07-14"),ee.Date("2022-07-15"))
+                .filterDate(ee.Date("2022-07-19"),ee.Date("2022-07-20"))
     
-    # testing output from python
-    #print("Sample modis output from python script: ")
-    #print(t_modis.size().getInfo())
-
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     # Get the population centre subset layer from a user's assets
     popctr_subset = ee.FeatureCollection("projects/patrickgosztonyi-lst/assets/subset_gpc_000b21a_e_WGS84")
@@ -38,36 +31,26 @@ def test_eeBatchExport(google_drive_folder):
     band_cast_dict = ee.Dictionary.fromLists(modis_band_names,new_types) #for each band, match it with the desired output type (for input into next command)
     t_modis_16bit = t_modis.cast(band_cast_dict,modis_band_names) # cast the bands according to the values we set is above three commands
 
-    # project the image to the equal-area projection
-
-    #get a script with the custom projection definition
-    # Import CRS from this script, See this script for links to source and description
-    #equalarea_proj = require('users/randd-eesd/dev:elijah/canada_wide_projections').ESRI_102001;
-    # manually define the projection info
-    equalarea_proj = 'PROJCS["Canada_Albers_Equal_Area_Conic", \
-                        GEOGCS["GCS_North_American_1983", \
-                            DATUM["D_North_American_1983", \
-                                SPHEROID["GRS_1980",6378137,298.257222101]], \
-                            PRIMEM["Greenwich",0], \
-                                UNIT["Degree",0.0174532925199433]], \
-                        PROJECTION["Albers"], \
-                        PARAMETER["Central_Meridian",-96], \
-                        PARAMETER["Latitude_Of_Origin",40], \
-                        PARAMETER["Standard_Parallel_1",50], \
-                        PARAMETER["Standard_Parallel_2",70], \
-                        PARAMETER["False_Easting",0], \
-                        PARAMETER["False_Northing",0], \
-                        UNIT["Meter",1], \
-                        AUTHORITY["EPSG","102001"]]'
-
-
-    # create a mappable function to reproject all images in the collection
-    def _reproject_image_collection(image):
-        
-        return image.reproject(equalarea_proj, None, image.projection().nominalScale())
-
-    # apply the reprojection function
-    t_modis_16bit_ea = t_modis_16bit.map(_reproject_image_collection)
+    #define the custom projection as a WKT string
+    """
+    # Readable format for the WKT projection
+    'PROJCS["Custom_Canada_Albers_Equal_Area_Conic", \
+         GEOGCS["GCS_North_American_1983", \
+             DATUM["D_North_American_1983", \
+                 SPHEROID["GRS_1980",6378137,298.257222101]], \
+             PRIMEM["Greenwich",0.0], \
+                 UNIT["Degree",0.0174532925199433]], \
+         PROJECTION["Albers Equal Area"], \
+         PARAMETER["central_meridian",-96.0], \
+         PARAMETER["lLatitude_of_origin",40.0], \
+         PARAMETER["Standard_Parallel_1",50.0], \
+         PARAMETER["Standard_Parallel_2",70.0], \
+         UNIT["Meter",1.0]]]'
+    """
+    wkt_customproj = 'PROJCS["Custom_Canada_Albers_Equal_Area_Conic", GEOGCS["GCS_North_American_1983", DATUM["D_North_American_1983", SPHEROID["GRS_1980",6378137,298.257222101]], PRIMEM["Greenwich",0.0], UNIT["Degree",0.0174532925199433]],PROJECTION["Albers Equal Area"], PARAMETER["Central_Meridian",-96.0], PARAMETER["Latitude_Of_Origin",40.0], PARAMETER["Standard_Parallel_1",50.0], PARAMETER["Standard_Parallel_2",70.0], UNIT["Meter",1.0]]]'
+    
+    # cast the project WKT string into an ee.Projection object
+    ee_customproj = ee.Projection(wkt_customproj)
 
     # need to clip the LST product to the population centre subset boundaries
     # do this by creating a function to clip and image, and then mapping it over the collection
@@ -88,10 +71,7 @@ def test_eeBatchExport(google_drive_folder):
     #end function
 
     # apply the clipping function
-    t_modis_16bit_clip = clip_image_collection(t_modis_16bit_ea,popctr_subset)
-
-    # will need to reproject the data, to an equal area projection (use the one Elijah created)
-    # t_modis_16bit_clip = ''#reproject
+    t_modis_16bit_clip = clip_image_collection(t_modis_16bit,popctr_subset)
 
     # get the IDs of each image in the collection, to use as index in the loop for exporting them
     image_ids = t_modis_16bit_clip.aggregate_array('system:index').getInfo()
@@ -106,6 +86,7 @@ def test_eeBatchExport(google_drive_folder):
             'folder'        : google_drive_folder, # 'earthengine/patrick', #folder names with separators (e.g. 'path/to/file') are interpreted as literal strings, not system paths.
             'scale'         : temp_image.projection().nominalScale().getInfo(), #1000 (metres) for testing, but will need to match the input dataset (926.625[...] or use the native resolution) 
             'region'        : popctr_subset.geometry().bounds(),
+            'crs'           : ee_customproj, #try and use the custom projection in the output
             'maxPixels'     : 1e10
             })
         temp_task.start()
